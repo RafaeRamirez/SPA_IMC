bb78ab76a7ba73adcb5d99910748188010cd011b
rafael augusto ramirez bolaño <rafar1129@gmail.com>
2025-06-17 19:41:58 -0500

    feat: the reame and main

--- src/js/main.js
+++ src/js/main.js
@@ -1,12 +1,16 @@
-
-
+const peso = Number(document.getElementById('peso').value)
+const altura = Number(document.getElementById('altura').value)
 function enviarFormulario(){
-            
-    const peso = Number(document.getElementById('peso').value)
-    const altura = Number(document.getElementById('altura').value)
+
+    let message
+const res = document.getElementById("resultado").innerHTML = peso / (altura * altura)
 
   if(!peso || !altura) return alert("Es necario colocar todos los valores")
+    if( res <18.5){
+            message ="Bajo de peso";
+    }
 
-  const res = document.getElementById("resultado").innerHTML = peso / (altura * altura)
+
+  
             
   } 
