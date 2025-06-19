5ff8e76c7fc87f69602854e6903eb4e4dec64d1d
rafael augusto ramirez bolaño <rafar1129@gmail.com>
2025-06-19 13:59:09 -0500

    feat: adding functionality

--- src/js/main.js
+++ src/js/main.js
@@ -1,16 +1,32 @@
-const peso = Number(document.getElementById('peso').value)
-const altura = Number(document.getElementById('altura').value)
+
+
+
 function enviarFormulario(){
 
-    let message
+   
+const peso = Number(document.getElementById('peso').value)
+const altura = Number(document.getElementById('altura').value)
 const res = document.getElementById("resultado").innerHTML = peso / (altura * altura)
 
-  if(!peso || !altura) return alert("Es necario colocar todos los valores")
-    if( res <18.5){
-            message ="Bajo de peso";
-    }
+let message
 
+if( res <18.5){
+  message ="Bajo de peso";
+} else if (res  <= 24.9){
+  message ="Peso normal";
+}
+else if (res >= 25 ){
+  message ="Sobrepeso";
+} else if (res >= 30 ){
+  message ="Obesidad grado 1";
+} else if (res >= 35){
+  message ="Obesidad grado 2";
+} else {
+  message ="Obesidad grado 3";
+}
+
+res.textContent="tu tienes un imc de "+ Math.round(res) +" por lo tanto esta en: "+ message
 
   
             
-  } 
+} 
