66dd7637e8306bffa4be7cacea6b51ccf643e1b4
rafael augusto ramirez bolaño <rafar1129@gmail.com>
2025-06-19 14:16:14 -0500

    feat: changing

--- src/js/main.js
+++ src/js/main.js
@@ -8,11 +8,6 @@ const peso = Number(document.getElementById('peso').value)
 const altura = Number(document.getElementById('altura').value)
 const res = document.getElementById("resultado").innerHTML = peso / (altura * altura)
 
-//let message
-
-
-//res.textContent="tu tienes un imc de "+ Math.round(res) +" por lo tanto esta en: "+ message
-
   
             
 } 
