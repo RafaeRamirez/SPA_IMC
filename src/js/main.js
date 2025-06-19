1283a8425b788343963e92681757beb3c1edfc99
rafael augusto ramirez bolaño <rafar1129@gmail.com>
2025-06-19 14:08:32 -0500

    feat: add photos

--- src/js/main.js
+++ src/js/main.js
@@ -10,20 +10,6 @@ const res = document.getElementById("resultado").innerHTML = peso / (altura * al
 
 //let message
 
-if( res <18.5){
-  message ="Bajo de peso";
-} else if (res  <= 24.9){
-  message ="Peso normal";
-}
-else if (res >= 25.56 ){
-  message ="Sobrepeso";
-} else if (res >= 30.67 ){
-  message ="Obesidad grado 1";
-} else if (res >= 35.67 ){
-  message ="Obesidad grado 2";
-} else {
-  message ="Obesidad grado 3";
-}
 
 //res.textContent="tu tienes un imc de "+ Math.round(res) +" por lo tanto esta en: "+ message
 
